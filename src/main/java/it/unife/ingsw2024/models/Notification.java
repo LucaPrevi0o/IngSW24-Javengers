c1b30659396da004b2f5e87d515b8313e1c1b42a
Luca_Previ0o <luca01.previati@edu.unife.it>
2024-08-29 11:28:52 +0200

    aggiunte funzionalità di base per le classi entità

--- src/main/java/it/unife/ingsw2024/models/Notification.java
+++ src/main/java/it/unife/ingsw2024/models/Notification.java
@@ -8,6 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name="NOTIFICATIONS")
 @Data
@@ -15,11 +19,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Notification {
 
-    @Id private int id; // notification unique id
+    @Id private int id; //notification unique id (i still don't know for sure how to implement the fact that it's automatically set like in MySQL)
     @OneToOne private User userSrc, userDst; //reference to User objects: the User account that generates the notification, the User account that receives the notification
     private String notificationMsg; //notification content
+    private Timestamp notificationDate; //notification date/time reference
     private boolean viewed; //reference for viewed notifications
 
-    //send a generic notification to a specific user
-    public static Notification send(int id, User userSrc, User userDst, String notificationMsg) { return new Notification(id, userSrc, userDst, notificationMsg, false); }
+    //send a generic notification to a specific user (timestamp is automatically set to the current time)
+    public static Notification send(int id, User userSrc, User userDst, String notificationMsg) { return new Notification(id, userSrc, userDst, notificationMsg, Timestamp.valueOf(LocalDateTime.now()), false); }
 }
--- src/main/java/it/unife/ingsw2024/models/User.java
+++ src/main/java/it/unife/ingsw2024/models/User.java
@@ -20,17 +20,43 @@ public class User {
     @ManyToMany private List<User> followers, followed; //list of User followers and followed accounts
     @OneToMany private List<Notification> recentNotifications; //list of notifications received
 
+    // --- --- ---
+
+    //for now this has no actual effect on the database (i think)
+    //at the same time, i'm not sure if this is the best way to tackle the problem, as i don't see any direct reference to the database update and i don't think this will actually work
+    //all of these function will probably have to be separated from the entity behaviour inside an actual Servlet controller (or whatever Spring uses to handle everything behind)
+    //but there's also a chance that Spring actually handles database communication without explicitly creating transactions and all of that, i'm not sure
+
+    // --- --- ---
+
     //mark all notifications as read - set all notification as not read
-    public void readAll() { for (var notification: recentNotifications) notification.setViewed(true); }
-    public void unreadAll() { for (var notification: recentNotifications) notification.setViewed(false); }
+    public void readAll() { for (var notification: this.recentNotifications) notification.setViewed(true); }
+    public void unreadAll() { for (var notification: this.recentNotifications) notification.setViewed(false); }
 
     //delete every viewed notifications from recent list
-    public void deleteReadNotifications() { recentNotifications.removeIf(Notification::isViewed); }
+    public void deleteReadNotifications() { this.recentNotifications.removeIf(Notification::isViewed); }
 
-    //follow/unfollow an account
-    public void follow(User user) { followers.add(user); }
-    public void unfollow(User user) { followers.remove(user); }
+    //follow an account
+    public void follow(User user) {
+
+        //notification message has to be written from the receiver's perspective
+        user.receiveNotification(Notification.send(0, this, user, this.username+" has followed you"));
+        this.followed.add(user); //add the followed account to the user's followed list
+        user.followers.add(user); //set this account as a new follower
+    }
+
+    //follow an account
+    public void unfollow(User user) {
+
+        //notification message has to be written from the receiver's perspective
+        user.receiveNotification(Notification.send(0, this, user, this.username+" has removed you from followers"));
+        this.followed.add(user); //remove the followed account to the user's followed list
+        user.followers.add(user); //unset this account as a follower
+    }
+
+    //send a message (this will only trigger a notification as message communication is not handled here)
+    public void sendTextMessage(User user, String message) { user.receiveNotification(Notification.send(0, this, user, this.username+" has sent you a message: "+message)); }
 
     //receive a generic notification
-    public void receiveNotification(Notification notification) { recentNotifications.add(notification); }
+    public void receiveNotification(Notification notification) { this.recentNotifications.add(notification); }
 }
