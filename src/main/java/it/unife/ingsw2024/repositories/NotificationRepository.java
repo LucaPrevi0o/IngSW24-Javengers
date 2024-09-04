274e69ba80efebecf66220e3dbc80a7261ba1ebb
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-09-04 11:19:07 +0200

    aggiunto ordinamento nella query

--- src/main/java/it/unife/ingsw2024/repositories/NotificationRepository.java
+++ src/main/java/it/unife/ingsw2024/repositories/NotificationRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 //notification DAO (list of allowed database operations, each one associated to a specific query)
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 
-    @Query(value="SELECT n FROM Notification n WHERE n.UserDst.id = ?1")
+    @Query(value="SELECT n FROM Notification n WHERE n.UserDst.id = ?1 order by n.NotificationDate desc, n.notificationTime desc")
     List<Notification> findAllByUserDst(int id);
+
+    @Query(value="SELECT n FROM Notification n order by n.NotificationDate desc, n.notificationTime desc")
+    List<Notification> findAll();
 }
