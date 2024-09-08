ce3c020e96a6405a066d31b954b8ea4b5ac44b55
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-09-08 10:33:27 +0200

    aggiunto mapping per utenti bloccati

---
+++ src/main/java/it/unife/ingsw2024/models/BlockedUserMapping.java
@@ -0,0 +1,26 @@
+package it.unife.ingsw2024.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="BLOCKED_USERS")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlockedUserMapping {
+
+    @Id @Column(name = "id")
+    private int id;
+
+    @Column(name = "BlockedUser")
+    private int blockedUserId;
+
+    @Column(name = "BlockerUser")
+    private int blockerUserId;
+}
