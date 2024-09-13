f6e5e2a33f32348449348cf44cbb36becefbcdc0
LucaPrevi0o <luca01.previati@edu.unife.it>
2024-09-13 15:15:10 +0200

    pulizia codice + commenti + rimozione following utente bloccato

--- src/main/java/it/unife/ingsw2024/web/RootController.java
+++ src/main/java/it/unife/ingsw2024/web/RootController.java
@@ -1,18 +1,10 @@
 package it.unife.ingsw2024.web;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import it.unife.ingsw2024.models.Notification;
 import it.unife.ingsw2024.models.User;
 import it.unife.ingsw2024.services.NotificationService;
 import it.unife.ingsw2024.services.UserService;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -27,18 +19,17 @@ import java.util.List;
 
 @Controller public class RootController {
 
-    @Autowired NotificationService notificationService;
-    @Autowired UserService userService;
-    @Autowired SimpMessagingTemplate simpMessagingTemplate;
+    @Autowired NotificationService notificationService; //service per gestione notifiche
+    @Autowired UserService userService; //service per gestione utenti
+    @Autowired SimpMessagingTemplate simpMessagingTemplate; //template per gestione notifiche push
 
-    private <T> boolean isContained(T usr, List<T> list) { return list.contains(usr); }
-
+    //funzione di controllo per verificare se l'utente destinatario può ricevere la notifica
     private boolean isNotificationReceivable(int usrDstId, Notification n) {
 
         var userPref=this.notificationService.getUserPreferences(usrDstId);
         var blockedUsersList=this.userService.getBlockedUsersList(usrDstId);
-        return !isContained(n.getUserSrc(), blockedUsersList) && (
-            n.getNotificationType()==0 && userPref.isMessages() ||
+        return !blockedUsersList.contains(n.getUserSrc()) && ( //controllo per verificare che l'utente non sia bloccato
+            n.getNotificationType()==0 && userPref.isMessages() || //controllo preferenze sulle categorie di notifiche
             n.getNotificationType()==1 && userPref.isFollowers() ||
             n.getNotificationType()==2 && userPref.isEvents() ||
             n.getNotificationType()==3 && userPref.isPayments());
@@ -47,7 +38,7 @@ import java.util.List;
     //da cancellare
     @MessageMapping("/application")
     @SendTo("/all/messages")
-    public Message send(final Message message) throws Exception { return message; }
+    public Message send(final Message message) { return message; } //
 
     //da cancellare
     /*@MessageMapping("/application/{userId}")
@@ -97,160 +88,122 @@ import java.util.List;
         return "sendNotificaTest";
     }
 
-    @RequestMapping("/")
-    public String helloWorld() { return "hello"; }
-
     @RequestMapping("/deleteAllRead")
     public ModelAndView deleteAllRead(@RequestParam int id) {
 
-        this.notificationService.deleteAllRead(id);
+        this.notificationService.deleteAllRead(id); //eliminazione di tutte le notifiche già lette
         var redirectView=new RedirectView("/getByUserId?id="+id);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina notifiche aggiornata
     }
 
     @RequestMapping("/getByUserId")
     public String getByUserId(Model model, @RequestParam int id) {
 
-        var notifications=this.notificationService.getAllByUserDstId(id);
-        notifications.removeIf(n -> !isNotificationReceivable(id, n));
-        var user=this.userService.getUserById(id);
+        var notifications=this.notificationService.getAllByUserDstId(id); //lettura lista notifiche indirizzate all'utente loggato
+        notifications.removeIf(n -> !isNotificationReceivable(id, n)); //cancellazione notifiche non visualizzabili
+        var user=this.userService.getUserById(id); //utente loggato
         model.addAttribute("notifications", notifications);
         model.addAttribute("user", user);
-        return "notifiche";
+        return "notifiche"; //redirection alla jsp "notifiche.jsp"
     }
 
     @RequestMapping("/setAllAsRead")
     public ModelAndView setAllAsRead(@RequestParam int id) {
 
-        var notifications=this.notificationService.getAllByUserDstId(id);
-        for (var n: notifications) n.setViewed(true);
-        this.notificationService.insertAll(notifications);
-
+        var notifications=this.notificationService.getAllByUserDstId(id); //lettura lista notifiche indirizzate all'utente loggato
+        for (var n: notifications) n.setViewed(true); //aggiornamento stato di lettura di ogni notifica
+        this.notificationService.insertAll(notifications); //aggiornamento notifiche all'interno del database
         var redirectView=new RedirectView("/getByUserId?id="+id);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina notifiche aggiornata
     }
 
     @RequestMapping("/follow")
     public ModelAndView followProfile(@RequestParam int id, @RequestParam int followedId) {
 
-        User userSrc = this.userService.getUserById(id);
-        User userDst = this.userService.getUserById(followedId);
+        var userSrc=this.userService.getUserById(id); //utente loggato
+        var userDst=this.userService.getUserById(followedId); //nuovo utente seguito dall'utente loggato
+        var blockedUsers=this.userService.getBlockedUsersList(followedId); //lista utenti bloccati dall'utente seguito
+        if (!blockedUsers.contains(userSrc)) { //il following del profilo è consentito solamente se l'utente seguito non ha bloccato l'utente loggato
 
-        var blockedUsers=this.userService.getBlockedUsersList(followedId);
-        if (!blockedUsers.contains(userSrc)){
-            this.userService.follow(id, followedId);
-            this.notificationService.sendNotification(userSrc, userDst, 1, "@<b>" + userSrc.getUsername() + "</b> ha cominciato a seguirti");
+            this.userService.follow(id, followedId); //aggiungi nuovo follower
+            this.notificationService.sendNotification(userSrc, userDst, 1, "@<b>"+userSrc.getUsername()+"</b> ha cominciato a seguirti"); //invia notifica
         }
-        var redirectView=new RedirectView("/following?id=" + followedId + "&loggedId=" + id);
-        return new ModelAndView(redirectView);
+        var redirectView=new RedirectView("/following?id="+followedId+"&loggedId="+id);
+        return new ModelAndView(redirectView); //redirection alla pagina profilo dell'utente seguito
     }
 
     @RequestMapping("/settings")
     public String settings(Model model, @RequestParam int id) {
 
-        var user=this.userService.getUserById(id);
-        var blockedUsers=this.userService.getBlockedUsersList(id);
-        var notifications=this.notificationService.getAllByUserDstId(id);
-        var userNotifPref=this.notificationService.getUserPreferences(id);
-        var notifPref=new boolean[]{userNotifPref.isMessages(), userNotifPref.isFollowers(), userNotifPref.isEvents(), userNotifPref.isPayments()};
-
+        var user=this.userService.getUserById(id); //utente loggato
+        var blockedUsers=this.userService.getBlockedUsersList(id); //lista utenti bloccati dell'utente bloccato
+        var notifications=this.notificationService.getAllByUserDstId(id); //lettura lista notifiche indirizzate all'utente loggato
+        var notifPref=this.notificationService.getUserPreferences(id).asList(); //lista preferenze notifiche utente loggato
         model.addAttribute("notifications", notifications);
         model.addAttribute("user", user);
         model.addAttribute("blockedUsers", blockedUsers);
         model.addAttribute("userPreferences", notifPref);
-
-        return "settings";
+        return "settings"; //redirection alla jsp "settings.jsp"
     }
 
     @RequestMapping("/updateSettings")
     public ModelAndView updateSettings(@RequestParam int id, @RequestParam boolean messages, @RequestParam boolean followers, @RequestParam boolean events, @RequestParam boolean payments) {
 
-        this.notificationService.updatePreferences(id, new boolean[]{messages, followers, events, payments});
+        this.notificationService.updatePreferences(id, new boolean[]{messages, followers, events, payments}); //aggiornamento preferenze notifiche utente
         var redirectView=new RedirectView("/settings?id="+id);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina impostazioni profilo aggiornata
     }
 
     @RequestMapping("/unfollow")
     public ModelAndView unfollowProfile(@RequestParam int id, @RequestParam int followedId) {
 
-        this.userService.unfollow(id, followedId);
-        var redirectView=new RedirectView("/following?id=" + followedId + "&loggedId=" + id);
-        return new ModelAndView(redirectView);
-    }
-
-    @RequestMapping("/notifiche")
-    public String notifiche(Model model, @RequestParam int id) {
-
-        var notifications=this.notificationService.getAll();
-        var user=this.userService.getUserById(id);
-        model.addAttribute("notifications", notifications);
-        model.addAttribute("user", user);
-        return "notifiche";
+        this.userService.unfollow(id, followedId); //rimozione following profilo utente
+        var redirectView=new RedirectView("/following?id="+followedId+"&loggedId="+id);
+        return new ModelAndView(redirectView); //redirection alla pagina profilo utente aggiornata
     }
 
     @RequestMapping("/notifclick")
     public ModelAndView viewandredirect(@RequestParam int id, @RequestParam int userId)  {
 
-        var toUpdate=this.notificationService.getById(id);
-        toUpdate.setViewed(true);
-        this.notificationService.insert(toUpdate);
-
+        var toUpdate=this.notificationService.getById(id); //lettura notifica in base a id
+        toUpdate.setViewed(true); //aggiornamento stato di lettura
+        this.notificationService.insert(toUpdate); //aggiornamento notifica letta
         var redirectView=new RedirectView("/getByUserId?id="+userId);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina notifiche aggiornata
     }
 
     @RequestMapping("/block")
     public ModelAndView block(@RequestParam int blockedId, @RequestParam int userId)  {
 
-        this.userService.block(blockedId, userId);
+        this.userService.unfollow(userId, blockedId); //rimozione following utente bloccato (quando l'utente viene bloccato perde in automatico il following dell'utente loggato)
+        this.userService.block(blockedId, userId); //aggiornamento stato di blocco dell'utente
         var redirectView=new RedirectView("/settings?id="+userId);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina impostazioni profilo con lista utenti bloccati aggiornata
     }
 
     @RequestMapping("/unblock")
     public ModelAndView unblock(@RequestParam int blockedId, @RequestParam int userId)  {
 
-        this.userService.unblock(blockedId, userId);
+        this.userService.unblock(blockedId, userId); //rimozione blocco utente
         var redirectView=new RedirectView("/settings?id="+userId);
-        return new ModelAndView(redirectView);
+        return new ModelAndView(redirectView); //redirection alla pagina impostazioni profilo con lista utenti bloccati aggiornata
     }
 
     @RequestMapping("/following")
     public String following(Model model, @RequestParam int id, @RequestParam int loggedId) {
 
-        /* Per la notification bell */
-        var notifications=this.notificationService.getAllByUserDstId(loggedId);
+        var notifications=this.notificationService.getAllByUserDstId(loggedId); //lista notifiche utente loggato
+        var selectedUser=this.userService.getUserById(id); //utente cercato nella pagina profilo
+        var user=this.userService.getUserById(loggedId); //utente loggato
+        var followerList=this.userService.getFollowerList(id); //lista follower utente cercato
+        var followedList=this.userService.getFollowedList(id); //lista utenti seguiti utente cercato
+        var blockedUsers=this.userService.getBlockedUsersList(loggedId); //lista utenti bloccati utente loggato
         model.addAttribute("notifications", notifications);
-
-        /* Per il display dell'username */
-        var selectedUser=this.userService.getUserById(id);
-        var user=this.userService.getUserById(loggedId);
-
-        /* Per il display del numero di followers/following */
-        var followerList=this.userService.getFollowerList(id);
-        var followedList=this.userService.getFollowedList(id);
         model.addAttribute("selectedUser", selectedUser);
         model.addAttribute("user", user);
         model.addAttribute("followerList", followerList);
         model.addAttribute("followedList", followedList);
-
-        /* Per il pulsante blocca/sblocca */
-        var blockedUsers=this.userService.getBlockedUsersList(loggedId);
         model.addAttribute("blockedUsers", blockedUsers);
-
-        return "following";
+        return "following"; //redirection alla jsp "following.jso"
     }
-
-    @RequestMapping("/2nd")
-    public String secondSubPage() { return "test/list"; }
-
-    @RequestMapping({"/testMysql"})
-    public String testMysql(Model model) {
-
-        model.addAttribute("test", this.notificationService.getAll());
-        return "testMysql";
-    }
-
-    @RequestMapping({"/testWithElements"})
-    public List<Notification> addElements() { return this.notificationService.addElements(); }
 }
